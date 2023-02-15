61267f6f0e35ec4e6905310d156182a22efc97f0
elif1986erdem <elif1986erdem@gmail.com>
2023-02-15 10:06:54 +0000

    security-group updated

--- blackbird/security_group.tf
+++ blackbird/security_group.tf
@@ -1,1 +1,45 @@
+resource "aws_security_group" "sample_tf" {
+  name        = "ssh-http-https"
+  description = "Allow SSH-HTTP-HTTPS for inbound traffic"
+  #vpc_id      = data.module.vpc.vpc_id
+  vpc_id = module.vpc.vpc_id
+  #vpc_id       = aws_vpc.main.id
 
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh-http-https"
+  }
+}
+
+data "aws_vpc" "vpc" {}
+  
