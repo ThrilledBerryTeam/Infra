75bc0c9c33f50528c273bee17567b3d3bc4e4679
SeherSezgin <ardaszgn16@gmail.com>
2023-03-02 12:01:15 +0300

    lambda funcation

--- Bluebird/iam.tf
+++ Bluebird/iam.tf
@@ -1,1 +1,47 @@
+resource "aws_iam_role" "lambda_assume" {
+  name = "lambda_assume"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+  })
+
+  tags = {
+    Company = "CompanyName"
+    Project = "EC2 Security"
+  }
+}
+
+resource "aws_iam_policy" "ec2_policy" {
+  name        = "ec2_policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "terraform_lambda_policy" {
+  role       = aws_iam_role.lambda_assume.name
+  policy_arn = aws_iam_policy.ec2_policy.arn
+}
+
+
+
+
+
 
---
+++ Bluebird/terminate_redundant_instances.py
@@ -0,0 +1,20 @@
+import boto3
+import re
+from datetime import datetime
+
+def lambda_handler(event, context):
+    TERMINATION_AGE = 0
+    ec2_client = boto3.client('ec2', region_name='eu-west-2')
+    # Get a list of stopped instances
+    instances = ec2_client.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            # StateTransitionReason might be like "i-xxxxxxxx User initiated (2016-05-23 17:27:19 GMT)"
+            reason = instance['StateTransitionReason']
+            date_string = re.search('User initiated \(([\d-]*)', reason).group(1)
+            if len(date_string) == 10:
+                date = datetime.strptime(date_string, '%Y-%m-%d')
+                # Terminate if older than TERMINATION_AGE
+                if (datetime.today() - date).days + 1 > TERMINATION_AGE:
+                    ec2_client.terminate_instances(InstanceIds=[instance['InstanceId']])
+            
